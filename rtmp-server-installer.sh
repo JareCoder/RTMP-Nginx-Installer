8e27767e513c1dce8d5e45220efa7d50e572b807
JareCoder <jesse.m.lappalainen@gmail.com>
2023-06-03 22:04:49 +0300

    New lines

--- rtmp-server-installer.sh
+++ rtmp-server-installer.sh
@@ -36,21 +36,21 @@ fi
 
 cd "$sources_dir"
 
-echo "Skipping functioning cloning and openssl installs"
+#echo "Skipping functioning cloning and openssl installs"
 
-echo -e "Trying to clone Nginx..."
+echo -e "\nTrying to clone Nginx..."
 try_clone "$nginx_stable_official" "$nginx_stable_fork" "$nginx_stable_official_branch" "$nginx_stable_fork_branch"
 
-echo -e "Trying to clone RTMP Module..."
+echo -e "\nTrying to clone RTMP Module..."
 try_clone "$rtmp_module_official" "$rtmp_module_fork"
 
-echo -e "Trying to clone OpenSSL..."
+echo -e "\nTrying to clone OpenSSL..."
 try_clone "$openssl_official" "$openssl_fork" "$openssl_official_branch" "$openssl_fork_branch"
 
-echo -e "Trying to install OpenSSL..."
+echo -e "\nTrying to install OpenSSL..."
 install_openssl "$sources_dir" "$working_dir" "$openssl_build_default"
 
-echo -e "Trying to install Nginx with RTMP module..."
+echo -e "\nTrying to install Nginx with RTMP module..."
 install_nginx_rtmp "$sources_dir" "$working_dir" "$nginx_rtmp_build_default"
 
 
