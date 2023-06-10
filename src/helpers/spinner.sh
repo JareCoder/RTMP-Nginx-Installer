1a459639c469527671d9576974c221df35d2b9c0
JareCoder <jesse.m.lappalainen@gmail.com>
2023-06-10 22:33:19 +0300

    Rewrote spinner

---
+++ src/helpers/spinner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#A basic loading animation.
+
+spinner(){
+    local spin='-\|/'
+
+    i=0
+    while true; do
+       i=$(( (i+1) %4 )) 
+       printf "\r${spin:$i:1}"
+       sleep .1
+    done
+}
