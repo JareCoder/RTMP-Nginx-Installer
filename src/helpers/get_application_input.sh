bdd1cd3eb5712edff2c22df349773779acba16f0
JareCoder <jesse.m.lappalainen@gmail.com>
2023-06-14 14:04:11 +0300

    Fixing prints

--- src/helpers/get_application_input.sh
+++ src/helpers/get_application_input.sh
@@ -2,17 +2,17 @@
 #Get application name from user
 #TODO Validate name. Not empty & no spaces
 get_application_input(){
-    printf '\nThe final from will be "rtmp:<ip>/<your input>/live(/<stream key>)". You can see the full address in the Information tab.\n' >&3
+    printf '\nThe final from will be "rtmp:<ip>/<your input>/live(/<stream key>)". You can see the full address in the Information tab.\n'
     while true; do
-        read -p $'\nInput application name (no spaces): ' app_name >&3
-        printf "Using $app_name." >&3
-        read -p '\nConfirm application name (Y/n): ' confirm_app >&3
+        read -p $'\nInput application name (no spaces): ' app_name
+        printf "Using $app_name."
+        read -p '\nConfirm application name (Y/n): ' confirm_app
         if [[ $confirm_app == [Yy] ]]; then
             break
         elif [[ $confirm_app == [Nn] ]]; then
-            printf "Re-input the name!\n" >&3
+            printf "Re-input the name!\n"
         else
-            printf "Invalid confirm input! Try again.\n" >&3
+            printf "Invalid confirm input! Try again.\n"
         fi
     done
 
--- src/helpers/get_ip_list_input.sh
+++ src/helpers/get_ip_list_input.sh
@@ -2,26 +2,26 @@
 #Get path or raw input of IPs
 get_ip_list_input(){
         while true; do
-        read -p $'\nChoose file path or manual input.\n1. File path\n2. Manual input' ip_list_input_type >&3
+        read -p $'\nChoose file path or manual input.\n1. File path\n2. Manual input' ip_list_input_type
         if [[ $ip_list_input_type == [1] ]]; then
             #Read file
-            read -p "Input file path: " ip_list_path >&3
+            read -p "Input file path: " ip_list_path
                 if [ -f "$ip_list_path" ]; then
-                    printf "Reading file...\n" >&3
+                    printf "Reading file...\n"
                     while IFS= read -r line; do
                         ip_lines+="$line"$'\n'
                     done < "$ip_list_path"
-                    printf "Done!\n" >&3
+                    printf "Done!\n"
                     break
                 else
-                    printf "File does not exist in given path. Try antoher path.\n" >&3
+                    printf "File does not exist in given path. Try antoher path.\n"
                 fi
         elif [[ $ip_list_input_type == [2] ]]; then
             #Read manual input
-            read -p 'Insert IPs with a space in between: ' -ra ip_list >&3
+            read -p 'Insert IPs with a space in between: ' -ra ip_list
             ip_lines=$(print "%s\n" "${ip_list[@]}")
         else
-            printf "Invalid input! Try again.\n" >&3
+            printf "Invalid input! Try again.\n"
             continue
         fi
     done
